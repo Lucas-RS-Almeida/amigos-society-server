3947a18b9add2645e57798b3b2dc0aae8bbf5b6a
Lucas Almeida <lucasrodolfo912@gmail.com>
2025-07-06 23:15:41 -0300

    update: routes to matches implemented

--- src/routes.ts
+++ src/routes.ts
@@ -6,18 +6,19 @@ import TeamController from "./app/controllers/TeamController";
 
 import auth from "./middlewares/auth";
 import checkUser from "./middlewares/checkUser";
+import MatchContoller from "./app/controllers/MatchContoller";
 
 const routes = Router();
 
 // Routes to authentication
-routes.post("/auth/log-in", AuthController.index as any);
-routes.post("/auth/sign-up", AuthController.store as any);
 routes.get(
   "/auth/check-user",
   auth as any,
   checkUser as any,
   AuthController.check,
 );
+routes.post("/auth/log-in", AuthController.index as any);
+routes.post("/auth/sign-up", AuthController.store as any);
 
 // Routes to players
 routes.get("/players/:matchDay", auth as any, PlayerController.index);
@@ -26,8 +27,32 @@ routes.post("/players", PlayerController.store as any);
 
 // Routes to teams
 routes.get("/teams", TeamController.index);
-routes.post("/teams", auth as any, TeamController.store as any);
-routes.put("/teams/:id", auth as any, TeamController.update as any);
-routes.delete("/teams/:id", auth as any, TeamController.delete);
+routes.post(
+  "/teams",
+  auth as any,
+  checkUser as any,
+  TeamController.store as any,
+);
+routes.put(
+  "/teams/:id",
+  auth as any,
+  checkUser as any,
+  TeamController.update as any,
+);
+routes.delete(
+  "/teams/:id",
+  auth as any,
+  checkUser as any,
+  TeamController.delete,
+);
+
+// Routes to matches
+routes.get("/matches", MatchContoller.index);
+routes.post(
+  "/matches",
+  auth as any,
+  checkUser as any,
+  MatchContoller.store as any,
+);
 
 export default routes;
